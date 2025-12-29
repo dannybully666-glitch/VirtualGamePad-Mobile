aeb4bb12f3488ac709b858c7e14735cbf61d635f
dannybully666-glitch <dannybully666@gmail.com>
2025-12-29 14:39:18 +0530

    Update MainActivity.kt

--- app/src/main/java/io/github/kitswas/virtualgamepadmobile/MainActivity.kt
+++ app/src/main/java/io/github/kitswas/virtualgamepadmobile/MainActivity.kt
@@ -3,134 +3,72 @@ package io.github.kitswas.virtualgamepadmobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import io.github.kitswas.virtualgamepadmobile.data.SettingsRepository
 import io.github.kitswas.virtualgamepadmobile.data.defaultBaseColor
 import io.github.kitswas.virtualgamepadmobile.data.defaultColorScheme
-import io.github.kitswas.virtualgamepadmobile.data.defaultHapticFeedbackEnabled
-import io.github.kitswas.virtualgamepadmobile.network.ConnectionViewModel
 import io.github.kitswas.virtualgamepadmobile.ui.screens.AboutScreen
-import io.github.kitswas.virtualgamepadmobile.ui.screens.ConnectMenu
-import io.github.kitswas.virtualgamepadmobile.ui.screens.ConnectingScreen
-import io.github.kitswas.virtualgamepadmobile.ui.screens.GamePad
 import io.github.kitswas.virtualgamepadmobile.ui.screens.MainMenu
 import io.github.kitswas.virtualgamepadmobile.ui.screens.SettingsScreen
 import io.github.kitswas.virtualgamepadmobile.ui.theme.VirtualGamePadMobileTheme
-import io.github.kitswas.virtualgamepadmobile.ui.utils.HapticUtils
-import kotlin.system.exitProcess
-
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val settingsRepository = SettingsRepository(this)
 
-        // Create a ViewModel the first time the system calls an activity's onCreate() method.
-        // Re-created activities receive the same ConnectionViewModel instance created by the first activity.
-        // Use the 'by viewModels()' Kotlin property delegate
-        // from the activity-ktx artifact
-        val connectionViewModel: ConnectionViewModel by viewModels()
         setContent {
-            AppUI(
-                connectionViewModel = connectionViewModel,
-                settingsRepository = settingsRepository,
-            )
+            AppUI(settingsRepository = settingsRepository)
         }
     }
 
     @Composable
     private fun AppUI(
-        connectionViewModel: ConnectionViewModel,
-        settingsRepository: SettingsRepository,
+        settingsRepository: SettingsRepository
     ) {
-        val hapticEnabled = settingsRepository.hapticFeedbackEnabled.collectAsState(
-            initial = defaultHapticFeedbackEnabled
-        )
-
-        LaunchedEffect(hapticEnabled.value) {
-            HapticUtils.isEnabled = hapticEnabled.value
-        }
-
         VirtualGamePadMobileTheme(
-            darkMode = settingsRepository.colorScheme.collectAsState(
-                initial = defaultColorScheme
-            ).value,
-            baseColor = settingsRepository.baseColor.collectAsState(
-                initial = defaultBaseColor
-            ).value
+            darkMode = settingsRepository.colorScheme.value ?: defaultColorScheme,
+            baseColor = settingsRepository.baseColor.value ?: defaultBaseColor
         ) {
-            NavTree(
-                connectionViewModel = connectionViewModel,
-                settingsRepository = settingsRepository,
-            )
+            NavTree(settingsRepository = settingsRepository)
         }
     }
 
     @Composable
     private fun NavTree(
-        connectionViewModel: ConnectionViewModel,
         settingsRepository: SettingsRepository,
-        navController: NavHostController = rememberNavController(),
+        navController: NavHostController = rememberNavController()
     ) {
-        NavHost(navController = navController, startDestination = "main_menu") {
+        NavHost(
+            navController = navController,
+            startDestination = "main_menu"
+        ) {
             composable("main_menu") {
                 MainMenu(
-                    onNavigateToConnectScreen = { navController.navigate("connect_screen") },
-                    onNavigateToSettingsScreen = { navController.navigate("settings_screen") },
-                    onNavigateToAboutScreen = { navController.navigate("about_screen") },
-                    onExit = { exitProcess(0) }
-                )
-            }
-            composable("connect_screen") {
-                ConnectMenu(
-                    onNavigateToConnectingScreen = { ipAddress, port ->
-                        navController.navigate("connecting_screen/$ipAddress/$port")
-                    }
-                )
-            }
-            composable(
-                "connecting_screen/{ipAddress}/{port}",
-                arguments = listOf(
-                    navArgument("ipAddress") { type = NavType.StringType },
-                    navArgument("port") { type = NavType.StringType }
-                )
-            ) { backStackEntry ->
-                val ipAddress = backStackEntry.arguments?.getString("ipAddress") ?: ""
-                val port = backStackEntry.arguments?.getString("port") ?: ""
-                ConnectingScreen(
-                    onNavigateToGamepad = {
-                        navController.navigate("gamepad") {
-                            popUpTo("connect_screen") { inclusive = true }
-                        }
+                    onNavigateToConnectScreen = {},
+                    onNavigateToSettingsScreen = {
+                        navController.navigate("settings_screen")
                     },
-                    onNavigateBack = { navController.popBackStack() },
-                    connectionViewModel = connectionViewModel,
-                    ipAddress = ipAddress,
-                    port = port
+                    onNavigateToAboutScreen = {
+                        navController.navigate("about_screen")
+                    },
+                    onExit = {}
                 )
             }
-            composable("gamepad") {
-                GamePad(
-                    connectionViewModel = connectionViewModel,
-                    onNavigateBack = { navController.popBackStack() }
-                )
-            }
+
             composable("settings_screen") {
                 SettingsScreen(
                     onNavigateBack = { navController.popBackStack() },
                     settingsRepository = settingsRepository
                 )
             }
+
             composable("about_screen") {
                 AboutScreen(
                     onNavigateBack = { navController.popBackStack() }
